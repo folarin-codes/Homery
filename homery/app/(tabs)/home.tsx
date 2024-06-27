327b9513207757897fbf3b8ad38fbaeeb905bb54
Adefolarin.. <suberufolarin@gmail.com>
2024-06-27 14:58:41 +0100

    added notification screen

---
+++ homery/app/(tabs)/home.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import {View , Text} from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = ()=>{
+    return(
+        <SafeAreaView style={{flex:1}}>
+            <StatusBar backgroundColor='red'/>
+            <Text style={{color:'red'}}>Home</Text>
+
+        </SafeAreaView>
+
+    )
+}
